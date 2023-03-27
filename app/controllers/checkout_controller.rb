2df5a2c19f68b0164be1bbd9a087f8ef702784cf
Fábio Rodrigues <fabio.rodrigues@runtime-revolution.com>
2023-03-27 09:22:36 +0100

    Checkout init

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,15 @@
 class CheckoutController < ApplicationController
 
   def create
+    ebook = Ebook.find(params[:id])
+    redirect_to ebooks_path if ebook.nil?
 
+    # Stripe API
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def success
